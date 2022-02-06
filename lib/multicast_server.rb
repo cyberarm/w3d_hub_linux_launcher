fd7c858bd86cc05de5a2b047dbf82974f42bf0ff
Cyberarm <matthewlikesrobots@gmail.com>
2022-02-06 09:29:40 -0600

    Stubbed multicast server, require ico parser

---
+++ lib/multicast_server.rb
@@ -0,0 +1,16 @@
+class W3DHub
+  # Maybe add remote game launch from server list app?
+  class MulticastServer
+    MULTICAST_ADDR = "224.87.51.68"
+    PORT = 7050
+
+    def initialize
+    end
+
+    def listen
+    end
+
+    def stop
+    end
+  end
+end
