33de050cd28e937ea822095ffe2fba67dcbf1eaa
Cyberarm <matthewlikesrobots@gmail.com>
2026-04-17 22:14:23 -0500

    Fixed capitalization error in app name

--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 module W3DHubLauncher
-  NAME = "Cyberarm's W3D Hub linux Launcher".freeze
+  NAME = "Cyberarm's W3D Hub Linux Launcher".freeze
   VERSION = "2.0.0".freeze
   VERSION_NAME = "alpha".freeze # TODO: Come up with a short, fun, release name
 end
