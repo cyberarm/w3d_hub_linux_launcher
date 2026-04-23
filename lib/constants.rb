f1d6bbe390ca6dd97174dc9a55f5362e310b08ff
Cyberarm <matthewlikesrobots@gmail.com>
2026-04-22 23:10:07 -0500

    Added some more paths

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,11 @@
 module W3DHubLauncher
+  DIRECTORY_NAME = "w3d-hub-linux-launcher".freeze
+
   ROOT_PATH = Dir.pwd
+  CONFIG_PATH = "#{Dir.home}/.config/#{DIRECTORY_NAME}".freeze
+  CACHE_PATH = "#{Dir.home}/.cache/#{DIRECTORY_NAME}".freeze
+  DEFAULT_PACKAGE_CACHE_PATH = "#{CACHE_PATH}/packages".freeze
+  DEFAULT_APPLICATIONS_PATH = "#{Dir.home}/.local/share/#{DIRECTORY_NAME}/applications".freeze
 
   USER_AGENT = "#{NAME} v#{VERSION}".freeze
 end
